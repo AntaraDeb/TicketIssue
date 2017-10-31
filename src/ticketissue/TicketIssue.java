4439193eb3572bf0a029bc54ba60772262ff0c7d
hp <hp@user>
2017-10-31 23:09:04 +0530

    updated commit

--- src/ticketissue/TicketIssue.java
+++ src/ticketissue/TicketIssue.java
@@ -1,58 +1,29 @@
 package ticketissue;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
 
-class PoliceOfficer {
-    String ticket;
-    String issueTicket(boolean b ){
+
+public class TicketIssue {
+    public static void main (String [] args) {
+        int speed= Integer.parseInt(args[0]);
+        boolean isBirthday = Boolean.parseBoolean(args[1]);
+        issueTicket(speed, isBirthday);
+    }
+    static void issueTicket(int speed, boolean isBirthday){
         int relaxation=0;
-        if (b==true){
+        if (isBirthday==true){
             relaxation=5;
         }
         System.out.println("Enter speed");
-        Scanner sc = new Scanner(System.in);
-        int speed= sc.nextInt();
         if(speed<=(60+relaxation)){
-            ticket="NO TICKET";
+            System.out.println("NO TICKET");
             }
-        else if(speed>=(61+relaxation)&&speed<=(80+relaxation)){
-            ticket="SMALL TICKET";
+        else if(speed>=(61+relaxation))
+        {
+            if(speed<=(80+relaxation))
+                System.out.println("SMALL TICKET");
+            else if(speed>=(81+relaxation))
+                System.out.println("BIG TICKET");       
         }
-        else if(speed>=(81+relaxation)){
-            ticket="BIG TICKET";
-        } 
-        return ticket;
+        
     }
-    
-}
-public class TicketIssue {
-    public static void main(String[] args) {
-            boolean loop;
-            boolean status=false;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter Name of Driver");
-            String name = sc.nextLine();
-            do{
-                loop=false;
-                try{
-                    System.out.println("Enter of Date Of Birth in format dd/mm/yyyy");
-                    SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM");
-                    Date dateOfBirth;
-                    dateOfBirth = dateformat.parse(sc.nextLine());
-                    Date todayDate = dateformat.parse(dateformat.format(new Date() ));
-                    if(dateOfBirth.equals(todayDate)){
-                        status= true;
-                    }    
-                } catch (ParseException ex) {
-                    System.out.println("Inavlid date Format.Enter in format dd/mm/yyyy");
-                    loop=true;
-                }
-            }while(loop);
-            PoliceOfficer po= new PoliceOfficer();
-            System.out.println("Hello "+name+".You have been issued "+po.issueTicket(status));           
-    }
-    
 }
